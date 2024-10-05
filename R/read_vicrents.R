73050369dd63d7a73908b572f95c95f27ebf0f0e
miguel ayala <miguel.ayala.au@gmail.com>
2024-10-05 12:13:06 +1100

    update

--- R/read_vicrents.R
+++ R/read_vicrents.R
@@ -1,3 +1,6 @@
+
+
+# packages --------
 library(readxl)
 library(tidyverse)
 library(lubridate)
@@ -5,49 +8,60 @@ library(janitor)
 library(fpp3)
 
 
-###### paths ############
+# url and file location ----------
 
 lga_rents_url <- "https://www.dffh.vic.gov.au/quarterly-median-rent-local-government-area-march-quarter-2024-excel"
   
 # "https://www.dffh.vic.gov.au/quarterly-median-rents-local-government-area-december-quarter-2023-excel"
-# "https://www.dffh.vic.gov.au/quarterly-median-rents-local-government-area-september-quarter-2023-excel"
-# "https://www.dffh.vic.gov.au/quarterly-median-rents-local-government-area-june-quarter-2023-excel"
 
-q <- Sys.Date() %>% yearquarter()-1
 
 rents_loc <- tempfile(fileext = ".xlsx", pattern = paste0("LGA_quarterly_median_rents_","2024 Q1_"), tmpdir = "C:\\Users\\MAyala\\Desktop\\shiny\\Housing db\\data-raw")
 
-#rents_loc <- "data-raw/LGA_quarterly_median_rents_2fe045c21063.xlsx"
+
+# if data is downloaded 
+rents_loc <- "data-raw/LGA_quarterly_median_rents_2024 Q1_48f4316c79bf.xlsx"
 #file <- "data-raw/LGA_quarterly_median_rents_2fe045c21063.xlsx"
 #sheet <- excel_sheets(rents_loc)[7]
 
-#### download and clean raw data #####
+
+
+# download and clean raw data ------
+
 download.file(url = lga_rents_url,
               destfile = rents_loc,
               mode = "wb")
 
 
 
-## function to read and clean rents raw data
+# function to read and clean rents raw data ----
+
 read_sheet <- function(file, sheet, area='LGA') {
+  
   raw_df <- suppressMessages(read_excel(file,
                                         sheet = sheet,
                                         skip = 1,
-                                        col_names = FALSE))
+                                        col_names = FALSE)
+                             )
   
   dates <- raw_df[1, 3:ncol(raw_df)] |>
     pivot_longer(cols = everything(),
                  names_to = "col",
                  values_to = "date") |>
     fill(date) %>% 
-    dplyr::mutate(date = case_when(date == "Dec 2003" & row_number() < 31 ~ "Dec 2002",#fix error in excel file (duplicate of quarter date)
-                            TRUE ~ date))
+    dplyr::mutate(
+      date = case_when(
+        #fix error in excel file (duplicate of quarter date)
+        date == "Dec 2003" & row_number() < 31 ~ "Dec 2002",
+        TRUE ~ date)
+      )
+  
   
   col_names <- raw_df |>
     filter(row_number() == 2) |>
     pivot_longer(everything(),
                  names_to = "col",
                  values_to = "series")
+  
   
   if(area=='LGA'){
     tidy_df <- raw_df |>
@@ -60,8 +74,10 @@ read_sheet <- function(file, sheet, area='LGA') {
       dplyr::select(-col) |>
       dplyr::rename(region = 1,
              lga = 2) |>
-      dplyr::mutate(value = suppressWarnings(as.numeric(value)),
-             date = lubridate::dmy(paste0("01 ", date)))
+      dplyr::mutate(
+        value = suppressWarnings(as.numeric(value)),
+        date = lubridate::dmy(paste0("01 ", date))
+        )
   }else{
     
     tidy_df <- raw_df |>
@@ -74,18 +90,25 @@ read_sheet <- function(file, sheet, area='LGA') {
       select(-col) |>
       dplyr::rename(region = 1,
              suburb = 2) |>
-      dplyr::mutate(value = suppressWarnings(as.numeric(value)),
-             date = lubridate::dmy(paste0("01 ", date)))
+      dplyr::mutate(
+        value = suppressWarnings(as.numeric(value)),
+        date = lubridate::dmy(paste0("01 ", date))
+        )
   }
   
   tidy_df$dwelling_type <- sheet
   
   tidy_df <- tidy_df %>% 
-    dplyr::mutate(region = case_when(region == "METRO NON-METRO" & lga == "Metro" ~ "Metro",
-                                     region == "METRO NON-METRO" & lga == "Non-Metro" ~ "Non-Metro",
-                                     region == "METRO NON-METRO" & lga == "Victoria" ~ "All",
-                                   TRUE ~ region),
-                  lga = case_when(lga == "Metro" ~ "Greater Melbourne",
+    dplyr::mutate(
+      
+      region = case_when(
+        region == "METRO NON-METRO" & lga == "Metro" ~ "Metro",
+        region == "METRO NON-METRO" & lga == "Non-Metro" ~ "Non-Metro",
+        region == "METRO NON-METRO" & lga == "Victoria" ~ "All",
+        TRUE ~ region
+        ),
+      lga = case_when(
+        lga == "Metro" ~ "Greater Melbourne",
                                   lga == "Non-Metro" ~ "Regional Victoria",
                                   TRUE ~ lga)) %>% 
     filter(!region %in% c("Table Total","METRO NON-METRO"))
@@ -93,9 +116,10 @@ read_sheet <- function(file, sheet, area='LGA') {
   tidy_df
 }
 
+
+# extract tab names
 sheets <- excel_sheets(rents_loc)
 
-#read_sheet(rents_loc,sheet = sheets[1])
 
 #compiles all rents data
 vic_rents <- map_dfr(.x = sheets, area = "LGA",
@@ -105,8 +129,9 @@ vic_rents <- map_dfr(.x = sheets, area = "LGA",
 
 #convert to tsibble
 vic_rents_ts <- vic_rents |>  
-  as_tsibble(key = c(lga,dwelling_type, series, region),
-             index = date) %>% 
+  as_tsibble(
+    key = c(lga, dwelling_type, series, region),
+    index = date) %>% 
   mutate(date = as.Date(date))
 
 
@@ -116,4 +141,16 @@ vic_rents_ts <- vic_rents |>
 
 #export clean data
 write_csv(vic_rents_ts,"data/Median Weekly Rents_202403.csv")
+
+
+
+# check data ------
+
+a <- vic_rents %>% 
+  filter(
+    series == "Median",
+    dwelling_type == "All Properties",
+    !lga %in% c("Group Total", "Greater Melbourne", "Regional Victoria", "Victoria")
+  )
+
 
